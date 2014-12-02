cdf30beba08b08ffed9ba45652b8bf71af7d15f6
boo1ean <boo1ean0807@gmail.com>
2014-12-03 00:07:04 +0200

    Tweak builder, add options soft-deletes

--- index.js
+++ index.js
@@ -6,63 +6,14 @@ function build (opts) {
 	var knex = opts.knex;
 	var table = opts.table;
 	var fields = _.isArray(opts.fields) ? opts.fields : null;
+	var softDeletes = opts.softDeletes;
 
 	return {
-		create: function create (data) {
-			return knex(table)
-				.insert(attrs(data))
-				.returning('id')
-				.then(_.first);
-		},
-
-		update: function update (data) {
-			if (!data || !data.id) {
-				throw new Error('Update operation requires id');
-			}
-
-			return knex(table)
-				.where('id', data.id)
-				.update(attrs(data))
-				.returning('id')
-				.then(_.first);
-		},
-
-		remove: function remove (criteria) {
-			return knex(table)
-				.where(prepareCriteria(criteria))
-				.update({ removed_at: 'now' })
-				.returning('id')
-				.then(_.first);
-		},
-
-		find: function find (criteria) {
-			return knex
-				.first('*')
-				.from(table)
-				.where(prepareCriteria(criteria));
-		},
-
-		query: function query (criteria, opts) {
-			criteria = criteria || {};
-
-			var query = knex
-				.select('*')
-				.from(table)
-				.where(criteria);
-
-			// Check out additional options
-			if (opts) {
-				if (opts.limit && opts.limit > 0) {
-					query.limit(opts.limit);
-				}
-
-				if (opts.offset && opts.offset > 0) {
-					query.offset(opts.offset);
-				}
-			}
-
-			return query;
-		}
+		create: getCreateMethod(),
+		update: getUpdateMethod(),
+		remove: getRemoveMethod(),
+		find: getFindMethod(),
+		query: getQueryMethod()
 	};
 
 	// Check if options are ok
@@ -77,6 +28,82 @@ function build (opts) {
 
 		if (!opts.knex) {
 			throw new Error('knex instance is required');
+		}
+	}
+
+	function getCreateMethod () {
+		return function create (data) {
+			return knex(table)
+			.insert(attrs(data))
+			.returning('id')
+			.then(_.first);
+		}
+	}
+
+	function getUpdateMethod () {
+		return function update (data) {
+			if (!data || !data.id) {
+				throw new Error('Update operation requires id');
+			}
+
+			return knex(table)
+				.where('id', data.id)
+				.update(attrs(data))
+				.returning('id')
+				.then(_.first);
+		}
+	}
+
+	function getRemoveMethod () {
+		if (softDeletes) {
+			return function remove (criteria) {
+				return knex(table)
+					.where(prepareCriteria(criteria))
+					.update({ removed_at: 'now' })
+					.returning('id')
+					.then(_.first);
+			}
+		} else {
+			return function remove (criteria) {
+				return knex(table)
+					.where(prepareCriteria(criteria))
+					.del()
+					.returning('id')
+					.then(_.first);
+			}
+		}
+	}
+
+	function getFindMethod () {
+		return function find (criteria) {
+			return knex
+			.first('*')
+			.from(table)
+			.where(prepareCriteria(criteria));
+		}
+	}
+
+	function getQueryMethod () {
+		return function query (criteria, opts) {
+			criteria = criteria || {};
+
+			var query = knex
+			.select('*')
+			.from(table)
+			.where(criteria);
+
+			// Check out additional options
+			if (opts) {
+				if (opts.limit && opts.limit > 0) {
+					query.limit(opts.limit);
+				}
+
+				if (opts.offset && opts.offset > 0) {
+					query.offset(opts.offset);
+				}
+			}
+
+			return query;
 		}
 	}
 
